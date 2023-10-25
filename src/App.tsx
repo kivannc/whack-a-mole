a82ce3924713a7b153e850c7ed063b4ebd4289db
Kivanc ERTURK <erturkkivanc@gmail.com>
2023-10-25 22:31:24 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import hole from './assets/hole.png';
+import mole from './assets/mole.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid">
+      <img src={hole} alt="" />
+      <img src={hole} alt="" />
+      <img src={hole} alt="" />
+      <img src={hole} alt="" />
+      <img src={hole} alt="" />
+      <img src={hole} alt="" />
+      <img src={hole} alt="" />
+      <img src={hole} alt="" />
+      <img src={hole} alt="" />
+    </div>
+  );
+}
+
+export default App;
